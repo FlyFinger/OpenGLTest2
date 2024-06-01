a592abff977f6209a25ad0f54cac3791473d41cc
军 曹 <669681710@qq.com>
2024-06-02 00:19:43 +0800

    v3

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-	printf("hello world!\n");
-	printf("hello world!\n");
-	printf("hello world!\n");
+	printf("hello world1!\n");
+	printf("hello world2!\n");
+	printf("hello world3!\n");
 	return 0;
 }
